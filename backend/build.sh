9c859a51ee7e8436cdc9694767d96eaf52e67b8a
surya teja <suryatejaqwe@gmail.com>
2026-04-18 00:52:51 +0530

    Fix Render deploy: Move migrate to start.sh

--- backend/build.sh
+++ backend/build.sh
@@ -5,16 +5,3 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate --no-input
-
-# Seed demo data only if the User table is empty (first deploy)
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if User.objects.count() == 0:
-    from django.core.management import call_command
-    call_command('seed_demo', '--force')
-    print('Seeded demo data')
-else:
-    print(f'DB already has {User.objects.count()} users, skipping seed')
-"
